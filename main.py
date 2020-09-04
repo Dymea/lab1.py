07b516c2f36693a00703bcb275fc9dc6b85ae141
Dymea <dxs5940@psu.edu>
2020-09-04 00:17:06 +0000

    Added 'else' to statement

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+# Author: Dymea Schippers dxs5940@psu.edu
+# Collaborator: Tyler Ciuca tmc6093@psu.edu
+# Collaborator: Chetan Mitra czm5805@psu.edu
+# Collaborator: Jian Hong WOng jpw6087@psu.edu
+
 temp = input("Enter temperature in celsius: ")
 unit = input("Enter unit in F/f or C/c: ")
 
-if unit == "F" or unit == "f":
+if unit == "C" or unit == "c":
+    celsius = float(temp)
+    print(f"{celsius}° in Celsius is equivalent to {celsius * 1.8 + 32}° Fahrenheit.")
+
+elif unit == "F" or unit == "f":
   fahrenheit = float(temp)
   print(f"{fahrenheit}° in Fahrenheit is equivalent to {(fahrenheit-32)*5/9}° Celsius.")
 
-  if unit == "C" or unit == "c":
-    celsius = float(temp)
-    print(f"{celsius}° in Celsius is equivalent to {celsius * 1.8 + 32}° Fahrenheit.")
+else :
+  print(f"Invalid unit({unit}).")
+
